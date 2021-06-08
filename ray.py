f82162ef1dd4c5bebeea8f41ca4b8c6b18e0f4c2
stakikawa <suzuran.takikawa@gmail.com>
2021-06-07 17:42:33 -0700

    Implemented ray class

---
+++ ray.py
@@ -0,0 +1,11 @@
+from vec3 import Vec3
+from point3 import Point3
+
+class Ray:
+
+    def __init__(self, origin, direction):
+        self.origin = origin
+        self.direction = direction
+
+    def at(self, t):
+        return self.origin + t*self.direction
