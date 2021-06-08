08f31c8c58a10c048d22aa5929b000f98db90c69
stakikawa <suzuran.takikawa@gmail.com>
2021-06-07 18:47:01 -0700

    Added ray color gradient and camera

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 from color import Color
 from color import write_color
+from ray import Ray
+from point3 import Point3
+from vec3 import Vec3
+from vec3 import unit_vector
+import math
+
+def ray_color(r):
+    unit_direction = unit_vector(r.direction)
+    t = 0.5*(unit_direction.y() + 1.0)
+    return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0)
 
 def main():
 
     # Image
-    IMAGE_HEIGHT = 256
-    IMAGE_WIDTH = 256
+    aspect_ratio = 16.0 / 9.0
+    image_width = 400
+    image_height = math.floor(image_width / aspect_ratio)
+
+    # Camera
+    viewport_height = 2.0
+    viewport_width = aspect_ratio * viewport_height
+    focal_length = 1.0
+
+    origin = Point3(0, 0, 0)
+    horizontal = Vec3(viewport_width, 0, 0)
+    vertical = Vec3(0, viewport_height, 0)
+    lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length)
 
     # Render
-    print("P3\n", IMAGE_WIDTH, ' ', IMAGE_HEIGHT, "\n255\n")
+    print("P3\n", image_width, ' ', image_height, "\n255\n")
 
-    for j in range(IMAGE_HEIGHT-1, -1, -1):
-        for i in range(IMAGE_WIDTH):
-            pixel_color = Color(float(i)/(IMAGE_WIDTH-1), float(j)/(IMAGE_HEIGHT-1), 0.25)
+    for j in range(image_height-1, -1, -1):
+        for i in range(image_width):
+            u = float(i) / (image_width - 1)
+            v = float(j) / (image_height - 1)
+            r = Ray(origin, lower_left_corner + u * horizontal + v * vertical - origin)
+            pixel_color = ray_color(r)
             write_color(pixel_color)
+
 main()
