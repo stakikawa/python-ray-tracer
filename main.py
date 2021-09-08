b2a0f9601a6fbabb74f7afad5c808421c8b5743d
stakikawa <suzuran.takikawa@gmail.com>
2021-09-08 13:18:23 -0700

    Ray coloring by sphere normal

--- main.py
+++ main.py
@@ -7,23 +7,30 @@ from vec3 import unit_vector
 from vec3 import dot
 import math
 
+
 def hit_sphere(center, radius, r):
     oc = r.origin - center
     a = dot(r.direction, r.direction)
     b = 2.0 * dot(oc, r.direction)
     c = dot(oc, oc) - radius * radius
-    discriminant = b*b - 4*a*c
-    return discriminant > 0
+    discriminant = b * b - 4 * a * c
+    if discriminant < 0:
+        return -1.0
+    else:
+        return (-b - math.sqrt(discriminant)) / (2.0 * a)
+
 
 def ray_color(r):
-    if hit_sphere(Point3(0, 0, -1), 0.5, r):
-        return Color(1, 0, 0)
+    t = hit_sphere(Point3(0, 0, -1), 0.5, r)
+    if t > 0.0:
+        n = unit_vector(r.at(t) - Vec3(0, 0, -1))
+        return 0.5 * Color(n.x() + 1, n.y() + 1, n.z() + 1)
     unit_direction = unit_vector(r.direction)
-    t = 0.5*(unit_direction.y() + 1.0)
+    t = 0.5 * (unit_direction.y() + 1.0)
     return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0)
 
-def main():
 
+def main():
     # Image
     aspect_ratio = 16.0 / 9.0
     image_width = 400
@@ -37,17 +44,18 @@ def main():
     origin = Point3(0, 0, 0)
     horizontal = Vec3(viewport_width, 0, 0)
     vertical = Vec3(0, viewport_height, 0)
-    lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length)
+    lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3(0, 0, focal_length)
 
     # Render
     print("P3\n", image_width, ' ', image_height, "\n255\n")
 
-    for j in range(image_height-1, -1, -1):
+    for j in range(image_height - 1, -1, -1):
         for i in range(image_width):
             u = float(i) / (image_width - 1)
             v = float(j) / (image_height - 1)
             r = Ray(origin, lower_left_corner + u * horizontal + v * vertical - origin)
             pixel_color = ray_color(r)
             write_color(pixel_color)
+
 
 main()
