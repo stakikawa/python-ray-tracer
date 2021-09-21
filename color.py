a06ef551f8097ce44d3d3576835e9f43faad3702
stakikawa <suzuran.takikawa@gmail.com>
2021-09-21 02:27:19 -0700

    Add gamma correction with gamma=2

--- color.py
+++ color.py
@@ -1,6 +1,6 @@
 from vec3 import Vec3
 from utility import clamp
-
+import math
 
 class Color(Vec3):
     pass
@@ -11,10 +11,11 @@ def write_color(pixel_color, samples_per_pixel=1):
     g = pixel_color.y()
     b = pixel_color.z()
 
+    # Divide color by number of samples and gamma correct with gamma = 2
     scale = 1.0 / samples_per_pixel
-    r *= scale
-    g *= scale
-    b *= scale
+    r = math.sqrt(r * scale)
+    g = math.sqrt(g * scale)
+    b = math.sqrt(b * scale)
 
     r = int(256 * clamp(r, 0.0, 0.999))
     g = int(256 * clamp(g, 0.0, 0.999))
