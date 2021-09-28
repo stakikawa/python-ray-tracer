55f370b325418a15757832b8400c261763aef23e
stakikawa <suzuran.takikawa@gmail.com>
2021-09-27 19:53:15 -0700

    Added fuzz to Metal material

--- metal.py
+++ metal.py
@@ -2,16 +2,18 @@ from material import Material
 from ray import Ray
 from vec3 import reflect
 from vec3 import unit_vector
+from vec3 import random_in_unit_sphere
 from vec3 import dot
 
 
 class Metal(Material):
 
-    def __init__(self, albedo):
+    def __init__(self, albedo, fuzz):
         self.albedo = albedo
+        self.fuzz = fuzz
 
     def scatter(self, r_in, rec):
         reflected = reflect(unit_vector(r_in.direction), rec.normal)
-        scattered = Ray(rec.p, reflected)
+        scattered = Ray(rec.p, reflected + self.fuzz*random_in_unit_sphere())
         attenuation = self.albedo
         return dot(scattered.direction(), rec.normal) > 0, attenuation, scattered
