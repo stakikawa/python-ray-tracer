4a5ffa9df1124b9b7a4a71d96d6c1422f8240209
stakikawa <suzuran.takikawa@gmail.com>
2021-10-05 18:13:00 -0700

    Updated main to run using PyTorch (WIP for optimization)

--- PyTorch/main.py
+++ PyTorch/main.py
@@ -1,34 +1,28 @@
-from Original.color import Color
-from Original.color import write_color
-from Original.hittable import HitRecord
-from Original.hittable_list import HittableList
-from Original.point3 import Point3
-from Original.sphere import Sphere
-from Original.vec3 import Vec3
-from Original.vec3 import unit_vector
-from Original.vec3 import random_vec
-from Original.vec3 import random_vec_mm
-from Original.lambertian import Lambertian
-from Original.dielectric import Dielectric
-from Original.metal import Metal
-from Original.camera import Camera
-from Original.utility import INFINITY
-from Original.utility import random_double
+from PyTorch.utility import write_color, unit_vector, length, INFINITY
+from PyTorch.utility import random_vec, random_vec_mm, random_double
+from PyTorch.hittable import HitRecord
+from PyTorch.hittable_list import HittableList
+from PyTorch.sphere import Sphere
+from PyTorch.lambertian import Lambertian
+from PyTorch.dielectric import Dielectric
+from PyTorch.metal import Metal
+from PyTorch.camera import Camera
 import math
+import torch
 
 
 def random_scene():
     world = HittableList()
 
-    ground_material = Lambertian(Color(0.5, 0.5, 0.5))
-    world.add(Sphere(Point3(0, -1000, 0), 1000, ground_material))
+    ground_material = Lambertian(torch.tensor([0.5, 0.5, 0.5]))
+    world.add(Sphere(torch.tensor([0, -1000, 0]), 1000, ground_material))
 
     for a in range(-11, 11):
         for b in range(-11, 11):
             choose_mat = random_double()
-            center = Point3(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double())
+            center = torch.tensor([a + 0.9 * random_double(), 0.2, b + 0.9 * random_double()])
 
-            if (center - Point3(4, 0.2, 0)).length() > 0.9:
+            if length(center - torch.tensor([4, 0.2, 0])) > 0.9:
 
                 if choose_mat < 0.8:
                     # diffuse
@@ -47,29 +41,35 @@ def random_scene():
                     world.add(Sphere(center, 0.2, sphere_material))
 
     material1 = Dielectric(1.5)
-    world.add(Sphere(Point3(0, 1, 0), 1.0, material1))
+    world.add(Sphere(torch.tensor([0.0, 1.0, 0.0]), 1.0, material1))
 
-    material2 = Lambertian(Color(0.4, 0.2, 0.1))
-    world.add(Sphere(Point3(-4, 1, 0), 1.0, material2))
+    material2 = Lambertian(torch.tensor([0.4, 0.2, 0.1]))
+    world.add(Sphere(torch.tensor([-4.0, 1.0, 0.0]), 1.0, material2))
 
-    material3 = Metal(Color(0.7, 0.6, 0.5))
-    world.add(Sphere(Point3(4, 1, 0), 1.0, material3))
+    material3 = Metal(torch.tensor([0.7, 0.6, 0.5]))
+    world.add(Sphere(torch.tensor([4.0, 1.0, 0.0]), 1.0, material3))
 
     return world
 
 
 def ray_color(r, world, depth):
-    rec = HitRecord()
-    if depth <= 0:
-        return Color(0, 0, 0)
-    if world.hit(r, 0.001, INFINITY, rec):
-        valid, attenuation, scattered = rec.material.scatter(r, rec)
-        if valid:
-            return attenuation * ray_color(scattered, world, depth - 1)
-        return Color(0, 0, 0)
-    unit_direction = unit_vector(r.direction)
-    t = 0.5 * (unit_direction.y() + 1.0)
-    return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0)
+    cur_ray = r
+    cur_attenuation = torch.ones(3)
+    for i in range(depth):
+        rec = HitRecord()
+        if world.hit(cur_ray, 0.001, INFINITY, rec):
+            valid, attenuation, scattered = rec.material.scatter(r, rec)
+            if valid:
+                cur_attenuation *= attenuation
+                cur_ray = scattered
+            else:
+                return torch.zeros(3)
+        else:
+            unit_direction = unit_vector(cur_ray.direction)
+            t = 0.5 * (unit_direction[1] + 1.0)
+            c = (1.0 - t) * torch.ones(3) + t * torch.tensor([0.5, 0.7, 1.0])
+            return cur_attenuation * c
+    return torch.zeros(3)
 
 
 def main():
@@ -84,9 +84,9 @@ def main():
     world = random_scene()
 
     # Camera
-    lookfrom = Point3(13, 2, 3)
-    lookat = Point3(0, 0, 0)
-    vup = Vec3(0, 1, 0)
+    lookfrom = torch.tensor([13.0, 2.0, 3.0])
+    lookat = torch.tensor([0.0, 0.0, 0.0])
+    vup = torch.tensor([0.0, 1.0, 0.0])
     dist_to_focus = 10.0
     aperture = 0.1
     fov = 20.0
@@ -94,25 +94,27 @@ def main():
     cam = Camera(lookfrom, lookat, vup, fov, aspect_ratio, aperture, dist_to_focus)
 
     # Render
-    anti_alias = True
-    print("P3\n", image_width, ' ', image_height, "\n255\n")
+    num_pixels = image_width * image_height
+    pixel_color = torch.zeros(num_pixels, 3)
 
     for j in range(image_height - 1, -1, -1):
         for i in range(image_width):
-            pixel_color = Color(0, 0, 0)
-            if anti_alias:
-                for s in range(samples_per_pixel):
-                    u = float(i + random_double()) / (image_width - 1)
-                    v = float(j + random_double()) / (image_height - 1)
-                    r = cam.get_ray(u, v)
-                    pixel_color += ray_color(r, world, max_depth)
-                write_color(pixel_color, samples_per_pixel)
-            else:
-                u = float(i) / (image_width - 1)
-                v = float(j) / (image_height - 1)
+            pixel_index = (j * image_width) + i
+            color = torch.zeros(3)
+            for s in range(samples_per_pixel):
+                u = (i + random_double()) / (image_width - 1)
+                v = (j + random_double()) / (image_height - 1)
                 r = cam.get_ray(u, v)
-                pixel_color += ray_color(r, world, max_depth)
-                write_color(pixel_color)
+                color += ray_color(r, world, max_depth)
+            color /= samples_per_pixel
+            color = torch.sqrt(color)
+            pixel_color[pixel_index] = color
 
+    # Write to File
+    print("P3\n", image_width, ' ', image_height, "\n255\n")
+    for j in range(image_height - 1, -1, -1):
+        for i in range(image_width):
+            pixel_index = (j * image_width) + i
+            write_color(pixel_color[pixel_index])
 
-main()
+# main()
